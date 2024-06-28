4ac73369ae0ba32112b15f45d1ad651bdb65c77e
palakknows <20215114.palak@gmail.com>
2024-06-28 14:38:12 +0530

    new one

---
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -0,0 +1,30 @@
+import { FormProvider, useForm } from "react-hook-form";
+import DetailsSection from "./DetailsSection";
+import TypeSection from "./TypeSection";
+
+export type HotelFormData={
+    name: string;
+    city: string;
+    country: string;
+    description :string;
+    type: string;
+    pricePerNight:number;
+    StarRating: number;
+    facilities: string[];
+    imageFiles: FileList;
+    adultCount: number;
+    childCount:number;
+}
+const ManageHotelForm = () => {
+    const formMethods=useForm<HotelFormData>();
+    return (
+    <FormProvider {...formMethods}>
+        <form className="flex flex-col gap-10">
+            <DetailsSection/>
+            <TypeSection/>
+            <FacilitiesSection/>
+        </form>
+        </FormProvider>
+    );
+};
+export default ManageHotelForm;
--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,5 +1,5 @@
 const AddHotel=()=>{
     return (<ManageHotelForm/>)
-}
+};
 
 export default AddHotel;
