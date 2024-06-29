e994faa934a0002ba3d5b5f973180e7d544ae0a0
palakknows <20215114.palak@gmail.com>
2024-06-29 19:08:30 +0530

    newbie

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,19 +2,20 @@ import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
-import express from "express";
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import path from "path";
 import authRoutes from "./routes/auth";
+import hotelRoutes from "./routes/hotels";
+import bookingRoutes from "./routes/my-bookings";
 import myHotelRoutes from "./routes/my-hotels";
 import userRoutes from "./routes/users";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -22,24 +23,27 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const frontendUrl = process.env.FRONTEND_URL || '';
-app.use(cors({
-    origin: frontendUrl,
-    credentials:true,
-})
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
 );
 
-app.use(express.static(path.join(__dirname,"../..frontend/dist")))
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-app.use("/api/users",userRoutes);
-app.use("/api/auth",authRoutes);
-app.use("/api/my-hotels",myHotelRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
+app.use("/api/hotels", hotelRoutes);
+app.use("/api/my-bookings", bookingRoutes);
 
-
-app.listen(7000, () => {
-    //server running on localhost:7000
-    console.log('server running on http://localhost:7000');
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
+app.listen(7000, () => {
+  console.log("server running on localhost:7000");
+});
 
 
